2c9a46c8cedf1316c778e0a19aae2d37b75e7dfc
thekiba <hr@thekiba.io>
2018-10-07 18:18:43 +0300

    feat(core): added *init directive
    An example:
    <ng-container *init="let myvar of 'lol'">{{ myvar }}</ng-container>

--- projects/platform/src/lib/directives/init.directive.ts
+++ projects/platform/src/lib/directives/init.directive.ts
@@ -1,21 +1,23 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef, EmbeddedViewRef } from '@angular/core';
 
 interface InitContext {
     $implicit: any;
-    init: any;
 }
 
-@Directive({selector: '[init]'})
+@Directive({ selector: '[init]' })
 export class InitDirective {
 
-    @Input() set init(val: any) {
-        this.viewContainer.clear();
-        this.viewContainer.createEmbeddedView(this.templateRef, {init: val});
+    @Input() set initOf(value: any) {
+        this.context.$implicit = value;
+        this.viewRef.markForCheck();
     }
+
+    private context: InitContext = { $implicit: null };
+    private viewRef: EmbeddedViewRef<InitContext> =
+        this.viewContainerRef.createEmbeddedView(this.templateRef, this.context);
 
     constructor(
         private templateRef: TemplateRef<InitContext>,
-        private viewContainer: ViewContainerRef
-    ) {
-    }
+        private viewContainerRef: ViewContainerRef
+    ) { }
 }
