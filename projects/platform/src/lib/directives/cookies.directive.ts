c472bc69a4000bea1f787a6700815688ee600029
Alex <dukhnovskiy.a@n-t.io>
2018-10-09 01:46:53 +0300

    feat(core): added *cookies directive
    CookiesDirective provides tempalte composition
    An Example:
    
    <ng-container *cookies="let cookie set 'ngxf' value 'Best Of The Best' path '/' domain: 'localhost' expires '01 Oct 2020'">
        {{ cookie }}
    </ng-container>
    
    <ng-container *cookies="let cookie get 'ngxf'">
        {{ cookie }}
    </ng-container>
    
    <ng-container *cookies="let cookie remove 'ngxf' path '/' domain: 'localhost'">
        {{ cookie }}
    </ng-container>

---
+++ projects/platform/src/lib/directives/cookies.directive.ts
@@ -0,0 +1,135 @@
+import { Directive, Input, TemplateRef, ViewContainerRef, EmbeddedViewRef, OnChanges, SimpleChanges } from '@angular/core';
+import { CookieOptionsArgs, CookiesService } from '../tools/cookies.tools';
+
+enum CookiesStrategies {
+    GET = 'get',
+    SET = 'set',
+    REMOVE = 'remove'
+}
+
+interface CookiesContext {
+    $implicit: any;
+    data: any;
+}
+
+interface CookiesStrategy {
+    type: CookiesStrategies;
+    changes: string[];
+    require: string[];
+}
+
+const COOKIES_CONFIG: CookiesStrategy[] = [
+    {
+        type: CookiesStrategies.GET,
+        changes: ['cookiesGet'],
+        require: ['cookiesGet']
+    },
+    {
+        type: CookiesStrategies.SET,
+        changes: [
+            'cookiesSet',
+            'cookiesValue',
+            'cookiesPath',
+            'cookiesDomain',
+            'cookiesExpires',
+            'cookiesSecure'
+        ],
+        require: ['cookiesSet']
+    },
+    {
+        type: CookiesStrategies.REMOVE,
+        changes: ['cookiesRemove', 'cookiesPath', 'cookiesDomain'],
+        require: ['cookiesRemove']
+    },
+];
+
+
+@Directive({ selector: '[cookies]' })
+export class CookiesDirective implements OnChanges {
+
+    @Input() private cookiesGet: string;
+    @Input() private cookiesSet: string;
+    @Input() private cookiesRemove: string;
+    @Input() private cookiesValue: any;
+
+    @Input() private cookiesPath: string;
+    @Input() private cookiesDomain: string;
+    @Input() private cookiesExpires: string | Date;
+    @Input() private cookiesSecure: boolean;
+
+    private context: CookiesContext = {
+        $implicit: null,
+        get data() {
+            return this.$implicit;
+        }
+    };
+
+    private viewRef: EmbeddedViewRef<CookiesContext> =
+        this.viewContainerRef.createEmbeddedView(this.templateRef, this.context);
+
+    constructor(
+        private templateRef: TemplateRef<CookiesContext>,
+        private viewContainerRef: ViewContainerRef,
+        private cookiesService: CookiesService
+    ) {
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        const strategy: CookiesStrategy = this.findStrategy(changes);
+
+        if (strategy) {
+            this.execute(strategy);
+        }
+    }
+
+    private findStrategy(changes: SimpleChanges): CookiesStrategy {
+        return COOKIES_CONFIG.find((strategy) => {
+            return strategy.changes.some(field => !!changes[field])
+                && strategy.require.every(field => !!this[field]);
+        });
+    }
+
+    private execute(strategy: CookiesStrategy): void {
+        const options = strategy.changes.map(field => this[field]);
+
+        this.action(strategy.type, ...options);
+    }
+
+    private action(type: string, ...options: string[]): void {
+        const name = options[0];
+        const value = options[1];
+        const cookieOptions: CookieOptionsArgs = {
+            path: options[2],
+            domain: options[3],
+            expires: options[4],
+            secure: !!options[5]
+        };
+
+        if (CookiesStrategies.GET === type) {
+            this.context.$implicit = this.getCookie(name);
+        }
+
+        if (CookiesStrategies.SET === type) {
+            this.setCookie(name, value, cookieOptions);
+            this.context.$implicit = value;
+        }
+
+        if (CookiesStrategies.REMOVE === type) {
+            this.removeCookie(name, cookieOptions);
+            this.context.$implicit = undefined;
+        }
+
+        this.viewRef.markForCheck();
+    }
+
+    private getCookie(name: string): string {
+        return this.cookiesService.get(name);
+    }
+
+    private setCookie(name: string, value: string, options?: CookieOptionsArgs): void {
+        this.cookiesService.set(name, value, options);
+    }
+    private removeCookie(name: string, options?: CookieOptionsArgs): void {
+        this.cookiesService.remove(name, options);
+    }
+}
